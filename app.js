32646357b180ab0018e571ff18c43515bd7884bb
MauStayHereForEverytime <maurichvz12@gmail.com>
2025-08-02 11:01:47 -0500

    Use of callback and function arrow

--- app.js
+++ app.js
@@ -11,9 +11,30 @@ function agregarAmigo() {
         input.value = ''; // Limpiar el input
     } else {
         if (!nombre) {
-            alert('Estás enviando un dato vacío.');
+            alert('Por favor inserte un nombre.');
         } else {
             alert('El nombre ya está en la lista.');
         }
     }
+}
+
+function mostrarAmigos() {
+    var lista = document.getElementById('lista');
+    lista.innerHTML = ''; // Limpiar la lista antes de mostrar los amigos
+    if (listaDeAmigos.length === 0) {
+        lista.innerHTML = '<li>No hay amigos agregados.</li>';
+    }
+    // Recorrer el array y crear un elemento li por cada amigo usando forEach y una función anónima o Callback
+    listaDeAmigos.forEach(function(amigo) {
+        var li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+    // Alternativamente, puedes usar una función flecha
+    /* listaDeAmigos.forEach(amigo => {
+    // Crear un elemento li por cada amigo
+    var li = document.createElement('li');
+    li.textContent = amigo;
+    lista.appendChild(li);
+}); */
 }
